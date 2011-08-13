d68aca79a95e62aad6f96ddcc740ba9262f48f0e
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2011-08-13 16:22:20 +0200

    - make the test app work

--- elasticlogger-test-app/src/org/hibnet/elasticlogger/testapp/Main.java
+++ elasticlogger-test-app/src/org/hibnet/elasticlogger/testapp/Main.java
@@ -1,5 +1,19 @@
 package org.hibnet.elasticlogger.testapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    public static void main(String[] args) throws Exception {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        while (true) {
+            logger.trace("test trace");
+            logger.debug("test debug");
+            logger.info("test info");
+            logger.warn("test warn");
+            logger.error("test error");
+            Thread.sleep(1000);
+        }
+    }
 }

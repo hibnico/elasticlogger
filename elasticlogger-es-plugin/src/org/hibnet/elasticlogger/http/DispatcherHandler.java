95ec95633f4d989306c75e904e9835c4a65df069
Nicolas Lalevée <nicolas.lalevee@hibnet.org>
2011-09-17 15:12:07 +0200

    trick for dev: reload dispatcher every time

--- elasticlogger-es-plugin/src/org/hibnet/elasticlogger/http/DispatcherHandler.java
+++ elasticlogger-es-plugin/src/org/hibnet/elasticlogger/http/DispatcherHandler.java
@@ -22,30 +22,42 @@ import org.elasticsearch.client.Client;
 import org.hibnet.elasticlogger.http.index.CreateIndexHandler;
 import org.hibnet.elasticlogger.http.index.DropIndexHandler;
 import org.hibnet.elasticlogger.http.index.IndexHandler;
-import org.hibnet.elasticlogger.http.search.SearchHandler;
+import org.hibnet.elasticlogger.http.logs.LogsHandler;
 
 public class DispatcherHandler extends AbstractHandlerContainer {
 
+    // in development, reload every time
+    private static final boolean RELOAD_ON_EACH_REQUEST = true;
+
     private Map<URIMatcher, Handler> handlers = new LinkedHashMap<URIMatcher, Handler>();
+    private final Client client;
 
     public DispatcherHandler(Client client) {
-        TemplateRenderer templateRenderer = new TemplateRenderer();
+        this.client = client;
+        handlers = loadHandlers();
+    }
 
+    private Map<URIMatcher, Handler> loadHandlers() {
+        Map<URIMatcher, Handler> newHandlers = new LinkedHashMap<URIMatcher, Handler>();
+
+        TemplateRenderer templateRenderer = new TemplateRenderer();
         IndexHandler indexHandler = new IndexHandler(client, templateRenderer);
-        handlers.put(eq("/"), indexHandler);
-        handlers.put(eq("/index.html"), indexHandler);
-        handlers.put(eq("/createIndex"), new CreateIndexHandler(client));
-        handlers.put(eq("/dropIndex"), new DropIndexHandler(client));
+        newHandlers.put(eq("/"), indexHandler);
+        newHandlers.put(eq("/index.html"), indexHandler);
+        newHandlers.put(eq("/createIndex"), new CreateIndexHandler(client));
+        newHandlers.put(eq("/dropIndex"), new DropIndexHandler(client));
 
         ResourceHandler resourceHandler = new ResourceHandler();
         resourceHandler.setBaseResource(Resource.newClassPathResource("/org/hibnet/elasticlogger/http/resources/"));
-        handlers.put(endsWith(".css"), resourceHandler);
-        handlers.put(endsWith(".png"), resourceHandler);
-        handlers.put(endsWith(".js"), resourceHandler);
+        newHandlers.put(endsWith(".css"), resourceHandler);
+        newHandlers.put(endsWith(".png"), resourceHandler);
+        newHandlers.put(endsWith(".js"), resourceHandler);
 
-        SearchHandler searchHandler = new SearchHandler(client, templateRenderer);
-        handlers.put(regexp("/([a-zA-Z][a-zA-Z0-9]*)/"), searchHandler);
-        handlers.put(regexp("/([a-zA-Z][a-zA-Z0-9]*)/index\\.html"), searchHandler);
+        LogsHandler searchHandler = new LogsHandler(client, templateRenderer);
+        newHandlers.put(regexp("/([a-zA-Z][a-zA-Z0-9]*)/"), searchHandler);
+        newHandlers.put(regexp("/([a-zA-Z][a-zA-Z0-9]*)/index\\.html"), searchHandler);
+
+        return newHandlers;
     }
 
     @Override
@@ -56,6 +68,9 @@ public class DispatcherHandler extends AbstractHandlerContainer {
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
+        if (RELOAD_ON_EACH_REQUEST) {
+            handlers = loadHandlers();
+        }
         for (Entry<URIMatcher, Handler> entry : handlers.entrySet()) {
             if (entry.getKey().match(target, request)) {
                 entry.getValue().handle(target, baseRequest, request, response);
